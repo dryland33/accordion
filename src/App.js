05bd700eb4a85ba93a330e5f1dde7dc09bdfa4d9
Rylan Wassem <dryland28@gmail.com>
2020-10-07 22:21:37 -0400

    accordion

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import React, { Component } from 'react';
-import HelloWorld from './state-drills/HelloWorld';
-import Bomb from './state-drills/Bomb';
-import RouletteGun from './state-drills/RouletteGun';
 import Accordion from './accordion/Accordion';
 import './App.css'
 
@@ -24,12 +21,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h2>Hello World</h2>
-        <HelloWorld />
-        <h2>Bomb</h2>
-        <Bomb />
-        <h2>Roulette Gun</h2>
-        <RouletteGun bulletInChamber={6} />
         <h2>Accordion</h2>
         <Accordion sections={sections} />
       </div>
